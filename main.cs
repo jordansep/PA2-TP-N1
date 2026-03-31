fe4f2960bc7799d3ce0cf903d42195e46294a5fc
jordansep <jordanmonier4@gmail.com>
2026-03-31 20:15:43 -0300

    Agregue el main

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hola Mundo");
+    }
+}
